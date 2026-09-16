34080448252320de5e9ffa724e66f06ed503e0e2
Septa Adi Nugroho <septaa811@gmail.com>
2026-09-16 08:49:02 +0700

    add docker config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "host.docker.internal",
   user: "root",
   password: "",
   database: "users_db1",
