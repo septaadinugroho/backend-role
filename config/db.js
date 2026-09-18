c7fa266853e98a1d2e77dbdb239fa3447d7924ac
Septa Adi Nugroho <septaa811@gmail.com>
2026-09-18 07:17:01 +0700

    add github actions ci

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "host.docker.internal",
-  user: "root",
-  password: "",
-  database: "users_db1",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "users_db1",
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 const db = pool.promise();
+
 module.exports = db;
+module.exports.pool = pool;
